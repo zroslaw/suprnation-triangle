5eea2ccd4f76f51bc57805f944adaca99258696d
Панасюк Ярослав Сергеевич <YSPanasyuk@sberbank.ru>
2022-12-01 16:01:50 +0300

    First running version on the base of Maven

--- src/main/java/mt/suprnation/trinagle/Main.java
+++ src/main/java/mt/suprnation/trinagle/Main.java
@@ -1,11 +1,74 @@
 package mt.suprnation.trinagle;
 
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Throwable{
 
+        // vars to gather time metrics
+        long t0 = System.currentTimeMillis(), t1, t2;
+
+        // the Triangle
+        List<int[]> triangle = new ArrayList<>();
+
+        // Read the input stream to the 2D array
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while(reader.ready()) {
+            String line = reader.readLine();
+            String[] stringArray = line.split(" ");
+            int[] row = new int[stringArray.length];
+            for (int i=0;i<stringArray.length;i++){
+                String stringNumber = stringArray[i];
+                row[i] = Integer.valueOf(stringNumber);
+            }
+            triangle.add(row);
+        }
+
+        t1 = System.currentTimeMillis();
+        // iterating from the bottom of the triangle to the top, row by row
+        Object[][] prevRowData = null;
+        for (int rowIndex=triangle.size()-1;rowIndex>-1;rowIndex--) {
+            int[] row = triangle.get(rowIndex);
+            Object[][] currentRowData = new Object[row.length][];
+            if (prevRowData==null){
+                // it is the first iteration, and we are at the bottom of triangle,
+                // initializing the currentRowData structure
+                for (int i=0;i<row.length;i++){
+                    List<Integer> minimalPath = new LinkedList<>();
+                    minimalPath.add(row[i]);
+                    currentRowData[i] = new Object[]{row[i],minimalPath};
+                }
+            }else{
+                // we are in the middle of the triangle,
+                // filling in currentRowData structure
+                for (int i=0;i<row.length;i++){
+                    // index of bottom left node - i,
+                    // index of bottom right node - i+1
+                    if ((Integer)prevRowData[i][0]<(Integer)prevRowData[i+1][0]){
+                        List<Integer> minimalPath = (List<Integer>)(prevRowData[i][1]);
+                        minimalPath.add(row[i]);
+                        currentRowData[i]=new Object[]{row[i]+(Integer)prevRowData[i][0],minimalPath};
+                    }else{
+                        List<Integer> minimalPath = (List<Integer>)(prevRowData[i+1][1]);
+                        minimalPath.add(row[i]);
+                        currentRowData[i]=new Object[]{row[i]+(Integer)prevRowData[i+1][0],minimalPath};
+                    }
+                }
+            }
+            prevRowData = currentRowData;
+        }
+
+        t2 = System.currentTimeMillis();
+
+        System.out.println(prevRowData[0][1]);
+
+        System.out.println("Reading input time: "+(t1-t0)/1000.+" seconds");
+        System.out.println("Processing triangle time: "+(t2-t1)/1000.+" seconds");
+
+
+        /**
         // ** Chapter 1 - Intuition and Abstract Thoughts **
         // Let's think clear and speak loudly.
         // We are given stream of arrays which represents rows of the triangle.
@@ -81,7 +144,7 @@ public class Main {
         // (like Concrete Mathematics), but for now it is important to just fulfilling the task at least in one way.
         //
         // Let's do it!
-
+        **/
 
     }
 
